3753f925a4e191f8cb81c221cd44791c58c3e517
Sophie Gu <sophieqgu@gmail.com>
2019-06-26 21:55:02 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,9 +1,9 @@
 class Show < ActiveRecord::Base 
-  def highest_rating
+  def self.highest_rating
     Show.maximum(:rating)
   end 
   
-  def most_popular_show
-    Show.where("rating  = ?", highest_rating)
+  def self.most_popular_show
+    Show.where("rating  = ?", self.highest_rating)
   end 
 end 
